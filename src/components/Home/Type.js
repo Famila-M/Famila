8d8f352d606a5aa3ac35b019d6666f5a28deadec
Fami <39043528+Famila-M@users.noreply.github.com>
2021-07-30 11:10:39 +0530

    commit

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,6 @@ function Type() {
         strings: [
           "Developer",
           "MERN Stack Developer",
-          "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
