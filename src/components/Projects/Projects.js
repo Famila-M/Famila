de4f1ffc4d9639eb13f072f1d4826e6f419b1732
Fami <39043528+Famila-M@users.noreply.github.com>
2024-02-19 14:32:33 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
+import travelapp from "../../Assets/Projects/Travel App.png"
 import grocery from "../../Assets/Projects/Grocery.png";
+import cafeking from "../../Assets/Projects/Cafe King.png";
+import bishoponchessboard from "../../Assets/Projects/Bishop on Chessboard.png";
 import colorpalatte from "../../Assets/Projects/colorpalatte.png";
 import zanzibarstore from "../../Assets/Projects/zanzi.png";
 import githubclone from "../../Assets/Projects/Github.png";
@@ -26,9 +29,25 @@ function Projects() {
               imgPath={zanzibarstore}
               isBlog={false}
               title="Zanzibar Store"
-              description="This is FullStack Ecommerce Store using ReactJs, NodeJs, JavaScript, BootStrap, HTML5 & CSS3."
+              description="Zanzibar Store is a comprehensive FullStack Ecommerce platform developed using 
+              ReactJs for the frontend, NodeJs for the backend, along with JavaScript, Bootstrap, HTML5, and 
+              CSS3, offering a seamless shopping experience with intuitive user interfaces, robust functionalities, 
+              and visually appealing design elements."
               link1="https://github.com/Famila-M/Ecommerce-Store"
               link2="https://zanzibar-store.netlify.app"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard 
+             imgPath={cafeking}
+             isBlog={false}
+             title="Cafe King"
+             description="CafeKing Web App, crafted with Materialize CSS, delivers a sleek and intuitive platform for 
+             coffee aficionados to explore, order, and engage with the coffee community, ensuring a seamless and enjoyable 
+             browsing experience."
+             link1="https://github.com/Famila-M/Materialize-CSS"
+             link2="https://cafeking-coffee.netlify.app/"
             />
           </Col>
 
@@ -76,6 +95,29 @@ function Projects() {
             />
           </Col>
 
+          <Col md={4} className="project-card">
+            <ProjectCard 
+             imgPath={travelapp}
+             isBlog={false}
+             title="Travel App"
+             description="The Travel App, designed in Figma, streamlines travel planning with its visually appealing interface, allowing users to discover destinations, book accommodations, and create itineraries seamlessly."
+             link2="https://www.figma.com/proto/BgN0la4LmYLlK4KeKXMIKF/Travel-App?node-id=1-3&starting-point-node-id=1%3A3&scaling=scale-down&mode=design&t=JTeUyGikiC51yTS8-1"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard 
+             imgPath={bishoponchessboard}
+             isBlog={false}
+             title="Bishop on Chessboard"
+             description="Bishop on Chessboard project is a program designed to simulate the movement of a bishop on a chessboard 
+             featuring a graphical user interface, movement validation, legal move highlighting, input validation, error handling, 
+             and customizable options, catering to both chess enthusiasts and programming enthusiasts."
+             link1="https://github.com/Famila-M/Bishop-on-Chessboard"
+             link2="https://bishoponchessboard.netlify.app/"
+            />
+          </Col>
+
         </Row>
       </Container>
     </Container>
@@ -83,3 +125,4 @@ function Projects() {
 }
 
 export default Projects;
+
