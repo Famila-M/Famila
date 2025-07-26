cab4851882c74ad23d63ffc41cd87113e9dc4b7d
Fami <39043528+Famila-M@users.noreply.github.com>
2025-07-26 16:39:16 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,12 +14,12 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There! <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+                Hey there! Glad you're here! <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
               </h1>
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Famila</strong>
+                <strong className="main-name"> Famila M</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
