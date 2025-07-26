1a60e5859361310c8272a1f7e15069634ebc9256
Fami <39043528+Famila-M@users.noreply.github.com>
2025-07-26 16:18:52 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,16 +10,20 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hello!!, I am <span className="purple">Famila </span>
             from <span className="purple"> Chennai, India.</span>
-            <br />An enthusiastic individual with a passion for technology and 
-            innovation. With a background in Full-Stack Development, particularly in the MERN stack, 
-            I thrive in dynamic environments where I can leverage my skills to create impactful solutions.
+            <br />A Frontend Developer with over 2+ years of experience building responsive and user-friendly web applications using 
+              Angular, React, JavaScript, and Spring Boot. I specialize in creating clean, scalable UI components and integrating secure, 
+                efficient backend services. Currently, I work at Promantus Inc, contributing to projects like DEWA and Material Cataloguing. 
+                I’m passionate about clean code, performance optimization, and delivering great user experiences.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
+              <li className="about-activity">
+              <ImPointRight /> Surfing Internet
+            </li>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Online Games
             </li>
             <li className="about-activity">
               <ImPointRight /> Reading Books
